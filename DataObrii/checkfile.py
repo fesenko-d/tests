fdb98e3e6bbdd3a7f8434467a546c441f8f2020f
fesenko-d <64193641+fesenko-d@users.noreply.github.com>
2024-12-26 14:49:30 +0200

    Update checkfile.py

--- DataObrii/checkfile.py
+++ DataObrii/checkfile.py
@@ -1,8 +1,8 @@
-import json
 import boto3
-import os
 
 def lambda_handler(event, context):
-    my_bucket = s3.Bucket('test-test-1111')
+    s3_resource = boto3.resource('s3')
+    my_bucket = s3_resource.Bucket('test-test-1111')
     for my_bucket_object in my_bucket.objects.all():
         print(my_bucket_object.key)
+
