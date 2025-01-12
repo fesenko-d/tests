6aea8d008412b3e1b05bf3e78b01d704c6516913
Dmytro Fesenko <dmitryfesenko92@gmail.com>
2025-01-12 03:36:17 +0200

    removing other project

--- DataObrii/checkfile.py
+++
@@ -1,8 +0,0 @@
-import boto3
-
-def lambda_handler(event, context):
-    s3_resource = boto3.resource('s3')
-    my_bucket = s3_resource.Bucket('test-test-1111')
-    for my_bucket_object in my_bucket.objects.all():
-        print(my_bucket_object.key)
-
